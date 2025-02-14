7637610427320c7f522bf5f5936dc550b41060a4
Haruto Ikehara <kduxwr@gmail.com>
2025-02-14 12:53:13 +0900

    Add AuthLayout test

---
+++ apps/frontend/src/__tests__/components/layouts/AuthLayout.spec.tsx
@@ -0,0 +1,53 @@
+import { AuthLayout } from "@/components/layouts/AuthLayout"
+import { removeToken, setToken } from "@/libs/utils/localStorage"
+import { mockToken } from "@/mocks/data/auth"
+import { AuthProvider } from "@/providers/auth"
+import { render, screen, waitFor } from "@testing-library/react"
+import { MemoryRouter, Route, Routes } from "react-router-dom"
+
+const mockedUseNavigate = vi.fn()
+vi.mock("react-router-dom", async () => {
+  const mod =
+    await vi.importActual<typeof import("react-router-dom")>("react-router-dom")
+  return {
+    ...mod,
+    useNavigate: () => mockedUseNavigate,
+  }
+})
+
+const renderAuthLayout = () => {
+  render(
+    <MemoryRouter initialEntries={["/auth"]}>
+      <AuthProvider>
+        <Routes>
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route index element={<div data-testid="child" />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </MemoryRouter>,
+  )
+}
+
+describe("AuthLayout", () => {
+  beforeEach(() => {
+    removeToken()
+  })
+
+  test("Should render the outlet when not logged in", async () => {
+    renderAuthLayout()
+
+    await waitFor(() => {
+      expect(screen.getByTestId("child")).toBeInTheDocument()
+    })
+  })
+
+  test("Should navigate to '/' when logged in", async () => {
+    setToken(mockToken)
+    renderAuthLayout()
+
+    await waitFor(() => {
+      expect(mockedUseNavigate).toHaveBeenCalledWith("/")
+    })
+  })
+})
